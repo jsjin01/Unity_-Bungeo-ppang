5fd8ae5808d0e7b369ff110eebb5f2a2f1aa3be6
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-03-16 01:24:44 +0900

    리로드

--- Bungeo-ppang/Assets/Script/PlayerMoveControl.cs
+++ Bungeo-ppang/Assets/Script/PlayerMoveControl.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PlayerMoveControl : MonoBehaviour
 {
-
-    Rigidbody2D rb;
+    [SerializeField] Rigidbody2D rb;
 
     [SerializeField] float speed = 5f;      //이동속도
 
     Vector3 movement;                       //move에서 사용할 변수 => 이동할 위치의 변수
-    // Start is called before the first frame update
+                                            // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -23,30 +22,16 @@ public class PlayerMoveControl : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {  
+    {
         Move(Input.GetAxisRaw("Horizontal"));
     }
 
     void Move(float x)
     {
-            movement.Set(x, 0, 0);
-            movement = movement.normalized * speed * Time.deltaTime;
-        
-        if (rb.transform.position.x < -2.6 ) 
-        {
-            rb.transform.position = new Vector3( -2.6f, -5.5f, 0);
-        }
-        else if(rb.transform.position.x > 2.6)
-        {
-            rb.transform.position = new Vector3(2.6f, -5.5f, 0);
-        }
-        else
-        {
-            rb.MovePosition(transform.position + movement);
-        }
-   
+        movement.Set(x, 0, 0);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + movement);
 
     }
-
-
 }
