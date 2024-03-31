1cdff6e2bbc0403c173aa6be1dbcade0574032c4
seohyun0030 <163545492+seohyun0030@users.noreply.github.com>
2024-03-31 22:06:52 +0900

    버튼 클릭

---
+++ Bungeo-ppang/Assets/Script/CardManager.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardManager : MonoBehaviour
+{
+    public void click(int num)
+    {
+        if (num == 0)
+        {
+            Debug.Log("붕어빵 달인");
+        }
+        else if (num == 1)
+        {
+            Debug.Log("배달이요");
+        }
+        else if (num == 2)
+        {
+            Debug.Log("이건 보너스");
+        }
+        else if (num == 3)
+        {
+            Debug.Log("따끈따끈 붕어빵");
+        }
+        else if (num == 4)
+        {
+            Debug.Log("식어버린 붕어빵");
+        }
+        else if (num == 5)
+        {
+            Debug.Log("왕 큰 붕어빵");
+        }
+        else if (num == 6)
+        {
+            Debug.Log("전사붕");
+        }
+        else if (num == 7)
+        {
+            Debug.Log("단단한 방패");
+        }
+        else if (num == 8)
+        {
+            Debug.Log("방패 강화");
+        }
+        else if (num == 9)
+        {
+            Debug.Log("반달 검격");
+        }
+        else if (num == 10)
+        {
+            Debug.Log("검격 강화");
+        }
+        else if (num == 11)
+        {
+            Debug.Log("연속 검격");
+        }
+        else if (num == 12)
+        {
+            Debug.Log("관통 검격");
+        }
+        else if (num == 13)
+        {
+            Debug.Log("전장 외침");
+        }
+        else if (num == 14)
+        {
+            Debug.Log("법사붕");
+        }
+        else if (num == 15)
+        {
+            Debug.Log("파이어 볼");
+        }
+        else if (num == 16)
+        {
+            Debug.Log("마법 강화 : 불");
+        }
+        else if (num == 17)
+        {
+            Debug.Log("캐스팅 감소 : 불");
+        }
+        else if (num == 18)
+        {
+            Debug.Log("아이스 볼");
+        }
+        else if (num == 19)
+        {
+            Debug.Log("마법 강화 : 얼음");
+        }
+        else if (num == 20)
+        {
+            Debug.Log("캐스팅 감소 : 얼음");
+        }
+        else if (num == 21)
+        {
+            Debug.Log("썬더 볼트");
+        }
+        else if (num == 22)
+        {
+            Debug.Log("깨달음 : 번개");
+        }
+        else if (num == 23)
+        {
+            Debug.Log("캐스팅 감소 : 번개");
+        }
+    }
+}
