9e7ac8ea5a865fb24bc3a4d87c5efadee373544a
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-04-03 02:14:26 +0900

    애니메이션 작업... 진행중

--- Bungeo-ppang/Assets/Script/Enemies.cs
+++ Bungeo-ppang/Assets/Script/Enemies.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemies : StandardEnemy
+public class Enemies : Enemy
 {
     [SerializeField] GameObject peanutslice_Prefebs;
     private Color originalcolor;
